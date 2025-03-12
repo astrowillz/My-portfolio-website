a4fba106a2a02bfa86d18f1ebf77d139d6708483
oluwasola <oluwasolakola@gmail.com>
2025-03-12 17:15:54 +0100

    new commit
    portfolio

---
+++ index2.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type",{
+    strings: ["Software developer.", "Web developer.","Front-end developer.", "Back-end developer.","Full-stack developer."],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop:true
+})
